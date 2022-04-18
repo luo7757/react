40634aa90f30d4e433ff4a4ba3d7c324b002a9e6
luo7757 <775743564@qq.com>
2022-04-18 10:59:45 +0800

    学习

--- React/学习记录/create-learn/src/app.jsx
+++ React/学习记录/create-learn/src/app.jsx
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-export default class app extends Component {
-
-  componentDidMount(){
-    const div = document.querySelector('.HTMLDiv');
-    div.onclick = (e) => {
-      console.log('真实dom HTMLDiv的事件,运行了')
-    }
-  }
-
+// 节点类型
+export default class App extends Component {
   render() {
-    let pre = null;
     return (
-      <div onClick={(e) => {
-        // console.log('div的事件，被阻止了')
-        console.log(e, pre, pre === e)
-      }}>
-        <div className='HTMLDiv'>
-          <button onClick={e => {
-            pre = e;
-            //e.persist();// 持久化对象 但好像在新版中 已经自动完成了持久化
-            // 在异步中使用 e对象 也能正确获取属性
-            // console.log('react元素事件，触发了')
-            // console.log(e.isPropagationStopped())
-            // e.stopPropagation()
-            // // 在 react 中 事件参数 e 根本就不是真实的 事件参数e，是一个合成参数
-            // // 它阻止的不是真实DOM的事件冒泡
-            // console.log(e.isPropagationStopped())
-
-            // console.log(e.nativeEvent);
-            // // 得到真实的DOM元素对象
-            console.log(e.type, "type")
-            // e.nativeEvent.stopImmediatePropagation()
-            setTimeout(() => {
-              console.log(e.type, "异步获取属性")
-            }, 0);
-          }}>点击</button>
-        </div>
-      </div> 
+      <div>
+        <ChildA />
+        <ChildB />
+        {ChildC}
+        {ChildD}
+        {ChildE}
+        {ChildF}
+      </div>
     )
   }
 }
 
-document.querySelector('#root').onclick = function(e){
-  console.log('真实的dom事件：id为root的div被点击了')
-  // e.stopPropagation();
+const ChildF = [2,3,4,5,6]
+// 数组节点
+
+const ChildE = null;
+// empty 节点
+
+const ChildD = '字符串节点'
+
+const ChildC = <div>dom节点</div>
+
+class ChildA extends Component{
+  render(){
+    return (
+      <div>类节点</div>
+    )
+  }
 }
+
+function ChildB() {
+  return (
+    <>
+      <div>函数节点</div>
+    </>
+  )
+}
+
--- React/学习记录/create-learn/src/index.jsx
+++ React/学习记录/create-learn/src/index.jsx
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import App from './app';
-// import Ref from './components/ref'
-
-// ReactDOM.render(<Ref />,document.getElementById("root"))
 
 ReactDOM.render(<App />,document.getElementById("root"))
-
-document.querySelector('#root').addEventListener('click', () => {
-  console.log('最终事件触发')
-})
