bf22e57ded3c94655f4e59ffd82861fc39edc81e
luo7757 <775743564@qq.com>
2022-04-19 12:14:18 +0800

    学习

--- React/学习记录/create-learn/src/app.jsx
+++ React/学习记录/create-learn/src/app.jsx
@@ -5,40 +5,10 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <ChildA />
-        <ChildB />
-        {ChildC}
-        {ChildD}
-        {ChildE}
-        {ChildF}
       </div>
     )
   }
 }
 
-const ChildF = [2,3,4,5,6]
-// 数组节点
 
-const ChildE = null;
-// empty 节点
-
-const ChildD = '字符串节点'
-
-const ChildC = <div>dom节点</div>
-
-class ChildA extends Component{
-  render(){
-    return (
-      <div>类节点</div>
-    )
-  }
-}
-
-function ChildB() {
-  return (
-    <>
-      <div>函数节点</div>
-    </>
-  )
-}
 
