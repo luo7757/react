92f3b1b891ec8f71eb32702c03a1f364fead6e82
luo7757 <775743564@qq.com>
2022-04-29 10:26:32 +0800

    学习

--- React/学习记录/create-learn/src/app.jsx
+++ React/学习记录/create-learn/src/app.jsx
@@ -1,67 +1,36 @@
-import React, { PureComponent, useRef, useImperativeHandle } from 'react'
-
-
-class Test extends PureComponent{
-
-  method() {
-    console.log('Test method is called')
-  }
-
+import React, { Component } from 'react'
+import { Routes, Route } from 'react-router-dom'
+export default class app extends Component {
   render() {
     return (
-      <div>
-        <h1>Test Component</h1>
-      </div>
+      <Routes>
+        <Route path="/a" element={<A />}></Route>
+        <Route path='/b' element={<B />}></Route>
+        <Route path='/c' element={<C />}></Route>
+      </Routes>
     )
   }
 }
-
-const Test2 = React.forwardRef(Test1)
-// 包装一个ref转发
-
-function Test1 (props, ref) {
-  // 函数组件不存在实例等东西 ,所以不能添加ref
-  // 同时为这个函数组件添加一些方法
-  const Ref = useRef(ref)
-  // 再次转发 ref
-
-  useImperativeHandle(ref, () => {
-    // 为指定 ref 对象 添加方法 返回的
-    // 使用 这个方法会替换掉 current对象
-    // current 指向这个函数返回的对象，不再指向 元素实例
-    // 可以再次转发 获取实例
-    return {
-      method() {
-        console.log('method')
-      },
-      current: Ref
-    }
-  }, [])
-  // 如果不给依赖项，则每次运行函数组件都会调用该方法
-  // 如果使用了依赖项，则第一次调用后，在依赖项没有发生变化时，不会再次调用
+function A(){
   return (
-    // 获得 元素实例
-    <div ref={Ref}>
-      Test1
+    <div>
+      mian组件
     </div>
   )
 }
 
-
-export default function App() {
-  const testRef = useRef();
-  // 创建一个 ref 对象
+function B(){
   return (
     <div>
-      {/* <Test ref={testRef} /> */}
-      <Test2 ref={testRef}/>
-      <button onClick={() => {
-        // Test2 传递ref 对象 转发到Test1中 做为第二个参数 在Test1 中接受，绑定到元素上
-        console.log(testRef.current)
-        // 但是在函数组件中 不存在实例方法等东西，那么想要为函数组件添加方法，目前就不行了
-        // ImperativeHandle Hook 就有作用了
-        testRef.current.method()
-      }}>点击调用Test组件的method方法</button>
+      组件B
+    </div>
+  )
+}
+
+function C(){
+  return (
+    <div>
+      组件C
     </div>
   )
 }
--- React/学习记录/create-learn/src/index.jsx
+++ React/学习记录/create-learn/src/index.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import App from './app';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+  
 
-ReactDOM.render(<App />,document.getElementById("root"))
+ReactDOM.render(<BrowserRouter>
+  <Routes>
+    <Route path="*" element={<App></App>}></Route>
+  </Routes>
+</BrowserRouter>,
+document.getElementById("root"))
