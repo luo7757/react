39ee7215a1658c26b36e10401b4a30bdf24097bb
luo7757 <775743564@qq.com>
2022-04-30 03:51:31 +0800

    学习

--- React/学习记录/create-learn/src/app.jsx
+++ React/学习记录/create-learn/src/app.jsx
@@ -1,24 +1,47 @@
 import React from 'react'
-import Layout from './components/Layout'
-import Title from './components/Title'
-import Menu from './components/Menu'
-import Main from './components/Main'
-import Login from './pages/Login'
-import { Routes, Route } from 'react-router-dom' 
-
-
-export default function app() {
+import { Routes, Route, useNavigate, useParams, useLocation } from 'react-router-dom'
+import qs from 'query-string'
+function A(props){
+  const navigate = useNavigate()
+  const params = useParams() // params 中提供了 v5 中 match 的数据
+  const location = useLocation()
   return (
     <div>
-      <Routes>
-        <Route path="*" element={<Layout 
-          title={<Title />} 
-          menu={<Menu />}
-          main={<Main />}
-          ></Layout>} />
-        <Route path="/login" element={<Login />}/>
-      </Routes>
-          
+      <div>组件A</div>
+      <button onClick={() => {
+
+        navigate('/b/2022/4/29', {state: '参数', replace: true})
+        // console.log('b', params, location, qs.parse(location.search))
+        // props.history.push("/b") v5老版本写法
+        // props.history.replace('/b','状态数据')
+        // console.log(props.match)
+      }}>go B</button>
     </div>
+  )
+}
+
+function B(props){
+  const navigate = useNavigate()
+  const params = useParams()
+  const location = useLocation()
+  return (
+    <div>
+      <div>组件B</div>
+      <button onClick={() => {
+        navigate('/', {'参数': 2})
+        console.log('A',params, location)
+      }}>go A</button>
+      <div>日期：{params.year}/{params.month}/{params.day}</div>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<A/>}></Route>
+      <Route path="/b/:year(\d+)/:month(\d+)/:day(\d+)" element={<B />}></Route>
+      {/* <Route path='/' element={<div>App</div>}></Route> */}
+    </Routes>
   )
 }
