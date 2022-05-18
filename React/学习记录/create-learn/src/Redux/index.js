a7ba0e4ef8a01506886b61c81f50410b90d14007
luo7757 <775743564@qq.com>
2022-05-19 01:04:29 +0800

    学习

--- React/学习记录/create-learn/src/Redux/index.js
+++ React/学习记录/create-learn/src/Redux/index.js
@@ -18,26 +18,28 @@ const combine = (state = {}, action) => {
 
 const store = createStore(combine);
 
-// const boundActions = bindActionCreators({
-//   createOutLoginUserAction,
-//   createSetLoginUserAction,
-//   createAddUserAction,
-//   createDeleteUserAction,
-//   createEditUserAction
-// }, store.dispatch)
+const boundActions = bindActionCreators({
+  createOutLoginUserAction,
+  createSetLoginUserAction,
+  createAddUserAction,
+  createDeleteUserAction,
+  createEditUserAction
+}, store.dispatch)
 
 // 
 
+// const store = createStore(User); // 运行了一次
+// Reducer 函数 在被注册的时候，会运行一次
 
-console.log(store)
+
 store.subscribe(() => {
   console.log(store.getState(),"状态改变")
 })
 
 
-// boundActions.createAddUserAction({name:"王刚", id: 3})
+boundActions.createAddUserAction({name:"王刚", id: 3})
 // boundActions.createAddUserAction({name:"小黄", id: 2})
-store.dispatch(createSetLoginUserAction({name: '小明', id: 1}))
+// store.dispatch(createSetLoginUserAction({name: '小明', id: 1})) // 数据变化导致全部reducer运行一次
 // dispatch 会遍历整个对象 传递dispatch中的type值，运行所有函数
 
 // store.dispatch(createAddUserAction({name: "小红", id: 1}))
