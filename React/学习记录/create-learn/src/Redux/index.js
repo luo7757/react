4c055272a981d5bb37f983c0483577e0d2582be6
luo7757 <775743564@qq.com>
2022-05-26 22:55:35 +0800

    学习

--- React/学习记录/create-learn/src/Redux/index.js
+++ React/学习记录/create-learn/src/Redux/index.js
@@ -10,33 +10,12 @@ import { createStore, bindActionCreators } from '../myRedux';
 // import { applyMiddleware } from 'redux';
 import applyMiddleware from '../myRedux/applyMiddleware';
 
-const Test1 = store => next => action => {
-  console.log('旧数据1', store.getState())
-  next(action)
-  console.log("改变方式1",action)
-  console.log("新数据1", store.getState())
-} 
+import logger from 'redux-logger'
+// logger 中间件 调试使用
 
-const Test2 = store => next => action => {
-  console.log('旧数据2', store.getState())
-  next(action)
-  console.log("改变方式2",action)
-  console.log("新数据2", store.getState())
-} 
-
-
-//const combine = (state = {}, action) => {
-//   return {
-//     loginUser: Login(state.LoginUserReducer, action),
-//     users: User(state.UsersReducer, action)
-//   }
-// } // combineReducers 就是返回一个对象
-
-// 通过applyMiddleware 进行中间件绑定
-const store = applyMiddleware(Test1, Test2)(createStore)(combine);
+const store = applyMiddleware(logger)(createStore)(combine);
 console.log(store)
 
-// const store = createStore(combine);
 
 const boundActions = bindActionCreators({
   createOutLoginUserAction,
@@ -46,25 +25,8 @@ const boundActions = bindActionCreators({
   createEditUserAction
 }, store.dispatch)
 
-// 
-// console.log(store, store.getState())
-
-// const store = createStore(User); // 运行了一次
-// Reducer 函数 在被注册的时候，会运行一次
-
-
-// store.subscribe(() => {
-//   console.log(store.getState(),"状态改变")
-// })
 
 
 boundActions.createAddUserAction({name:"王刚", id: 3})
 boundActions.createAddUserAction({name:"小明", id: 4})
-store.dispatch(createSetLoginUserAction({name: '小明', id: 1})) // 数据变化导致全部reducer运行一次
-// dispatch 会遍历整个对象 传递dispatch中的type值，运行所有函数
-
-// store.dispatch(createAddUserAction({name: "小红", id: 1}))
-// store.dispatch(createAddUserAction({name: "小黄", id: 2}))
-// store.dispatch({type: "WDADW", payload: {name:"test"}})
-// boundActions.createDeleteUserAction({id: 3})
-// boundActions.createEditUserAction({name: "修改", id: 1})
+store.dispatch(createSetLoginUserAction({name: '小明', id: 1}))
